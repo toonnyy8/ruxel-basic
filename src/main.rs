7bacab03d78e77532507466b1805c01c5f75f062
toonnyy8 <toonnyy8@gmail.com>
2021-03-02 22:23:40 +0800

    exit 流程

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,23 @@ use std::sync::Mutex;
 struct AppStateWithBasicData {
     cursor: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
 }
+struct CorePort {
+    port: String, // <- Mutex is necessary to mutate safely across threads
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let args: Vec<String> = std::env::args().collect();
+    let mut core_port = String::from("3030");
+    let mut port = String::from("3031");
+    for idx in 0..args.len() {
+        if args[idx] == "--core_port" {
+            core_port = args[idx + 1].clone();
+        } else if args[idx] == "--port" {
+            port = args[idx + 1].clone();
+        }
+    }
+
     let basic_data = web::Data::new(AppStateWithBasicData {
         cursor: Mutex::new(0),
     });
@@ -15,20 +29,24 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(basic_data.clone())
+            .data(CorePort {
+                port: core_port.clone(),
+            })
+            .route("/exit", web::post().to(exit_handler))
             .route("/get_cursor", web::post().to(get_cursor_handler))
             .route("/set_cursor", web::post().to(set_cursor_handler))
             .route("/cmd", web::post().to(cmd_handler))
     })
-    .bind("127.0.0.1:3031")?
+    .bind(format!("127.0.0.1:{}", port))?
     .run()
     .await
 }
 
-async fn cmd_handler(bytes: web::Bytes) -> Result<String> {
+async fn cmd_handler(core_port: web::Data<CorePort>, bytes: web::Bytes) -> Result<String> {
     let cmd = String::from(std::str::from_utf8(&bytes[..]).unwrap());
 
     let cursor = client::Client::new()
-        .post("http://localhost:3030/get_cursor")
+        .post(format!("http://localhost:{}/get_cursor", core_port.port))
         .send_body(Body::from_message(cmd.clone()))
         .await
         .unwrap()
@@ -39,7 +57,7 @@ async fn cmd_handler(bytes: web::Bytes) -> Result<String> {
     let cursor = i32::from_ne_bytes(cursor);
 
     client::Client::new()
-        .post("http://localhost:3030/print")
+        .post(format!("http://localhost:{}/print", core_port.port))
         .send_body(Body::from_message(format!("{}\n{}:", cmd, cursor)))
         .await
         .unwrap()
@@ -48,6 +66,12 @@ async fn cmd_handler(bytes: web::Bytes) -> Result<String> {
         .unwrap();
 
     Ok(format!("Welcome {}!", "info.username"))
+}
+
+async fn exit_handler(bytes: web::Bytes) -> Result<web::Bytes> {
+    println!("exit");
+    std::process::exit(0);
+    Ok(web::Bytes::new())
 }
 
 async fn get_cursor_handler(
